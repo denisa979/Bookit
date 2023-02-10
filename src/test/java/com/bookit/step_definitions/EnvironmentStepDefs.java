1172b6349ea57778bcffbcc260a688986d718f56
jamaldemir <jamal@cybertekschool.com>
2023-02-09 19:57:39 -0500

    hw added

--- src/test/java/com/bookit/step_definitions/EnvironmentStepDefs.java
+++ src/test/java/com/bookit/step_definitions/EnvironmentStepDefs.java
@@ -9,5 +9,10 @@ public class EnvironmentStepDefs {
         System.out.println("Environment.URL = " + Environment.URL);
         System.out.println("Environment.BASE_URL = " + Environment.BASE_URL);
         System.out.println("Environment.TEACHER_EMAIL = " + Environment.TEACHER_EMAIL);
+
+        String teacher_email = System.getProperty("teacher_email") != null ?
+                teacher_email = System.getProperty("teacher_email") : Environment.TEACHER_EMAIL;
+
+        System.out.println("teacher_email = " + teacher_email);
     }
 }
